44fa6a59ac5b49b8229e172a1781ed76227bd256
Hernan Arregoces <analista1.desarrollo@wplay.co>
2018-01-31 14:29:14 -0500

    initial commit

---
+++ src/main/java/com/backendlist/spring/backendlist/App.java
@@ -0,0 +1,13 @@
+package com.backendlist.spring.backendlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
